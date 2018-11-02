aeb89f162a2fa9a8bd12a14d877b1e342659c4c8
Leanor <leanor@yandex.ru>
2018-11-02 22:01:08 +0300

    Test Add contact added and refactored

---
+++ contact.py
@@ -0,0 +1,8 @@
+
+class Contact:
+
+    def __init__(self, first_name, middle_name, last_name, nick_name):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.nick_name = nick_name
