44180e86751c7ff526a04f3856c14de722f3e6d9
Leanor <leanor@yandex.ru>
2018-11-08 13:34:12 +0300

    Refactoring of Add_contact test by using fixture and pytest

--- application.py
+++ application.py
@@ -37,6 +37,26 @@ class Application:
         # return to groups page
         self.return_to_groups_page()
 
+    def add_contact(self, Contact):
+        wd = self.wd
+        # click on add new
+        wd.find_element_by_link_text("add new").click()
+        # fill in form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(Contact.first_name)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(Contact.middle_name)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(Contact.last_name)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(Contact.nick_name)
+        # submit form
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+
     def open_groups_page(self):
         wd = self.wd
         wd.find_element_by_link_text("groups").click()
