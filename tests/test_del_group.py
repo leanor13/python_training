06d7dae162c2b75b0f23128261eb597f2e5ef230
Leanor <leanor@yandex.ru>
2018-11-11 21:37:02 +0300

    Fixture is moved from different tests to conftest.py

--- tests/test_del_group.py
+++ tests/test_del_group.py
@@ -1,13 +1,3 @@
-import pytest
-from model.group import Group
-from fixture.application import Application
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
-
 
 def test_delete_first_group(app):
     app.session.login(username="admin", password="secret")
