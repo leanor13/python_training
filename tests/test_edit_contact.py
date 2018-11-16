5973c1c38d849362fb79a22478f71e55422c6585
leanor13 <yulia.ioffe@genestack.com>
2018-11-16 15:25:22 +0300

    "edit contact" test refactored again - split to 4 tests

--- tests/test_edit_contact.py
+++ tests/test_edit_contact.py
@@ -2,6 +2,18 @@
 from model.contact import Contact
 
 
-def test_first_contact_editing(app):
-    app.contact.edit_first_contact(Contact(first_name="Edited Contact First", middle_name="Edited Contact Mid", last_name="Edited Last", nick_name="Edited Nick"))
+def test_first_contact_editing_first_name(app):
+    app.contact.edit_first_contact(Contact(first_name="Edited Contact First 2"))
+
+
+def test_first_contact_editing_middle_name(app):
+    app.contact.edit_first_contact(Contact(middle_name="Edited Contact Mid 2"))
+
+
+def test_first_contact_editing_last_name(app):
+    app.contact.edit_first_contact(Contact(last_name="Edited Last 2"))
+
+
+def test_first_contact_editing_nick_name(app):
+    app.contact.edit_first_contact(Contact(nick_name="Edited Nick 2"))
 
