bafee58dad0abc084204e87361539a7564b55aeb
leanor13 <yulia.ioffe@genestack.com>
2018-11-16 20:43:04 +0300

    added checks of list length for groups

--- model/group.py
+++ model/group.py
@@ -2,7 +2,8 @@
 
 class Group:
 
-    def __init__(self, gr_name=None, gr_header=None, gr_footer=None):
+    def __init__(self, gr_name=None, gr_header=None, gr_footer=None, gr_id=None):
         self.gr_name = gr_name
         self.gr_header = gr_header
         self.gr_footer = gr_footer
+        self.id = gr_id
--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -3,9 +3,15 @@ from model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(gr_name="First Test Group", gr_header="Test", gr_footer="Test footer"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
 def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(gr_name="", gr_header="", gr_footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
