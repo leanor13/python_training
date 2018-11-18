fe2ae92d815eff7ae2dc1bdcf7ea80f7d020396a
Leanor <leanor@yandex.ru>
2018-11-18 18:14:14 +0300

    group lists are sorted, lists are compared

--- model/group.py
+++ model/group.py
@@ -6,4 +6,16 @@ class Group:
         self.gr_name = gr_name
         self.gr_header = gr_header
         self.gr_footer = gr_footer
-        self.id = gr_id
+        self.gr_id = gr_id
+
+    def __repr__(self):
+        return "%s:%s" % (self.gr_id, self.gr_name)
+
+    def __eq__(self, other):
+        return (self.gr_id is None or other.gr_id is None or self.gr_id == other.gr_id) \
+               and self.gr_name == other.gr_name
+
+
+
+
+
