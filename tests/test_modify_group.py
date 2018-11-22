e3ad04e6b2df4c644f792c6df69f91777c75c51b
leanor13 <yulia.ioffe@genestack.com>
2018-11-22 20:52:12 +0300

    hash for groups - delete and modify group

--- tests/test_modify_group.py
+++ tests/test_modify_group.py
@@ -6,10 +6,9 @@ def test_modify_group_name(app):
         app.group.create(Group(gr_name="Created for Group Modification Test"))
     old_groups = app.group.get_group_list()
     group = Group(gr_name="New group 2", gr_id=old_groups[0].gr_id)
-#    group.gr_id = old_groups[0].gr_id
     app.group.modify_first_group(group)
+    assert len(old_groups) == app.group.count()
     new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
     old_groups[0] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
