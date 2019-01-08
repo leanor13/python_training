3240fe3fb37deb44d832dfe52cfc3940b6780025
leanor13 <yulia.ioffe@genestack.com>
2019-01-08 17:35:23 +0300

    тестовые данные для групп загружаются из файла

---
+++ data/groups.py
@@ -0,0 +1,6 @@
+from model.group import Group
+
+testdata = [
+    Group(gr_name="name", gr_header="header", gr_footer="footer"),
+    Group(gr_name="name2", gr_header="header2", gr_footer="footer2")
+]
--- generator/group.py
+++ generator/group.py
@@ -2,7 +2,7 @@ from model.group import Group
 import random
 import string
 import os.path
-import json
+import jsonpickle
 import getopt
 import sys
 
@@ -13,7 +13,7 @@ except getopt.GetoptError as err:
     getopt.usage()
     sys.exit(2)
 
-n = 5
+n = 3
 f = "data/groups.json"
 
 for o, a in opts:
@@ -60,4 +60,7 @@ testdata_combination = [
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as out:
-    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))
+
+
