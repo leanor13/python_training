aad19626128705f2f5f0592689eba9ac00436bf0
leanor13 <yulia.ioffe@genestack.com>
2019-01-08 16:54:59 +0300

    сделан генератор данных параметры можно передавать через командную строку

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,44 +1,10 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 import pytest
-import random
-import string
+from data.add_group import constant as testdata
 
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*10
-    # generate string of random length and random symbols
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-def random_string_punctuation(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    # generate string of random length and random symbols
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Group(gr_name="", gr_header="", gr_footer="")] + [
-    Group(gr_name=random_string("name", 10), gr_header=random_string("header", 20), gr_footer=random_string("footer", 20))
-    for i in range(2)
-]
-
-
-testdata_combination = [
-    Group(gr_name=name, gr_header=header, gr_footer=footer)
-    for name in ["", random_string("name", 10)]
-    for header in ["", random_string("header", 20)]
-    for footer in ["", random_string("footer", 20)]
-]
-
-
-testdata_punctuation = [Group(gr_name="", gr_header="", gr_footer="")] + [
-    Group(gr_name=random_string_punctuation("name", 10), gr_header=random_string_punctuation("header", 20),
-          gr_footer=random_string_punctuation("footer", 20))
-    for i in range(5)
-]
-
-
-@pytest.mark.parametrize("group", testdata_punctuation, ids=[repr(x) for x in testdata])
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 def test_add_group(app, group):
     old_groups = app.group.get_group_list()
     app.group.create(group)
