84be0fcfff0ebe079ee3411162acff46f8931145
leanor13 <yulia.ioffe@genestack.com>
2019-01-10 18:34:18 +0300

    изменены тесты для добавления и удаления групп - ускорены за счет обращения к БД

--- fixture/group.py
+++ fixture/group.py
@@ -51,6 +51,17 @@ class GroupHelper:
         self.return_to_groups_page()
         self.group_cache = None
 
+    def delete_group_by_id(self, id):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        self.select_group_by_id(id)
+        # delete first group
+        wd.find_element_by_name("delete").click()
+        # return to groups page
+        self.return_to_groups_page()
+        self.group_cache = None
+
     def delete_first_group(self):
         self.delete_group_by_index(0)
 
@@ -68,6 +79,10 @@ class GroupHelper:
     def select_group_by_index(self, index):
         wd = self.app.wd
         wd.find_elements_by_name("selected[]")[index].click()
+
+    def select_group_by_id(self, id):
+        wd = self.app.wd
+        wd.find_element_by_css_selector("input[value='%s']" % id).click()
 
     def select_first_group(self):
         self.select_group_by_index(0)
