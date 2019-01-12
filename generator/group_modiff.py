1c615864da95e39c9f56de43e1aa992dbbcc5774
leanor13 <yulia.ioffe@genestack.com>
2019-01-12 19:45:22 +0300

    тест на модификацию групп теперь читает данные из json файла

---
+++ generator/group_modiff.py
@@ -0,0 +1,45 @@
+from model.group import Group
+import random
+import string
+import os.path
+import jsonpickle
+import getopt
+import sys
+
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f", ["number of groups", "file"])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+n = 3
+f = "data/groups_modiff.json"
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
+
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    # generate string of random length and random symbols
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [
+    Group(gr_name=random_string("Mod_name", 10), gr_header=random_string("Mod_header", 20),
+          gr_footer=random_string("Mod_footer", 20))
+    for i in range(n)
+]
+
+
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
+
+with open(file, "w") as out:
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))
+
+
--- tests/test_modify_group.py
+++ tests/test_modify_group.py
@@ -1,15 +1,16 @@
 from model.group import Group
 import random
-from generator.group import random_string
 
 
-def test_modify_group_name(app, db, check_ui):
+def test_modify_group(app, db, json_groups_modiff, check_ui):
+    group = json_groups_modiff
     if len(db.get_group_list()) == 0:
         app.group.create(Group(gr_name="Created for Group Modification Test"))
     old_groups = db.get_group_list()
+
+    # choose group that will be modified
     chosen_group = random.choice(old_groups)
-    group = Group(gr_name=random_string("Modiff", 13), gr_footer=random_string("Modiff", 13),
-                  gr_header=random_string("Modiff", 13), gr_id=chosen_group.gr_id)
+    group.gr_id = chosen_group.gr_id
     app.group.modify_group_by_id(chosen_group.gr_id, group)
     new_groups = db.get_group_list()
     old_groups.remove(chosen_group)
@@ -19,14 +20,5 @@ def test_modify_group_name(app, db, check_ui):
         def clean(gr):
             return Group(gr_id=gr.gr_id, gr_name=gr.gr_name.strip())
 
-        assert sorted(map(clean, new_groups), key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
-
-
-
-#def test_modify_group_header(app):
-#    if app.group.count() == 0:
-#        app.group.create(Group(gr_name="Created for Group Deletion Test"))
-#    old_groups = app.group.get_group_list()
-#    app.group.modify_first_group(Group(gr_header="New header"))
-#    new_groups = app.group.get_group_list()
-#    assert len(old_groups) == len(new_groups)
+        assert sorted(map(clean, new_groups), key=Group.id_or_max) == sorted(app.group.get_group_list(),
+                                                                             key=Group.id_or_max)
