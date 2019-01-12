01386b0f4d85aeb43db0d006f619a23ff2e816ae
leanor13 <yulia.ioffe@genestack.com>
2019-01-12 19:34:03 +0300

    все тесты на группы обновлены - добавлен параметр check_ui, если он отключен - проверяется только БД

--- tests/test_modify_group.py
+++ tests/test_modify_group.py
@@ -1,17 +1,26 @@
 from model.group import Group
-from random import random
+import random
+from generator.group import random_string
 
 
 def test_modify_group_name(app, db, check_ui):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(gr_name="Created for Group Modification Test"))
     old_groups = db.get_group_list()
-    id = random.choice(old_groups).gr_id
-    group = Group(gr_name="MODIFF", gr_id=old_groups[id].gr_id)
-    app.group.modify_group_by_id(id, group)
+    chosen_group = random.choice(old_groups)
+    group = Group(gr_name=random_string("Modiff", 13), gr_footer=random_string("Modiff", 13),
+                  gr_header=random_string("Modiff", 13), gr_id=chosen_group.gr_id)
+    app.group.modify_group_by_id(chosen_group.gr_id, group)
     new_groups = db.get_group_list()
-    old_groups[index] = group
-    assert old_groups == new_groups
+    old_groups.remove(chosen_group)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    if check_ui:
+        def clean(gr):
+            return Group(gr_id=gr.gr_id, gr_name=gr.gr_name.strip())
+
+        assert sorted(map(clean, new_groups), key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+
 
 
 #def test_modify_group_header(app):
